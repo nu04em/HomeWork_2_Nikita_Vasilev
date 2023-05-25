64334f1a04e5d030429fa27afb3d7d67df2fd702
Nikita <nikitarusvasilev@gmail.com>
2023-05-26 01:37:37 +0300

    update Task 15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,32 +1,49 @@
 ﻿// Напишите программу, которая принимает на вход цифру, 
 // обозначающую день недели, и проверяет, является ли этот день выходным.
 
+// Console.Clear();
+
+// Console.Write("Введите номер дня недели: ");
+// int n = int.Parse(Console.ReadLine()!);
+// while (n < 1 || n > 7)
+//     {
+//         Console.Write("Вы ошиблись!\nВведите номер дня недели: ");
+//         n = int.Parse(Console.ReadLine()!);
+//     }
+// while (n == 6 || n == 7)
+//     {
+//         Console.WriteLine("Выходной!");
+//         return;
+//     }
+
+// if (n == 1)
+//     Console.WriteLine("Понедельник");
+// else if (n == 2)
+//     Console.WriteLine("Вторник");
+// else if (n == 3)
+//     Console.WriteLine("Среда");
+// else if (n == 4)
+//     Console.WriteLine("Четверг");
+// else if (n == 5)
+//     Console.WriteLine("Пятница");
+
+    
+// 2 Вариант
+
 Console.Clear();
 
-Console.Write("Введите номер дня недели: ");
+Console.Write("Введите номер выходного дня недели: ");
 int n = int.Parse(Console.ReadLine()!);
 while (n < 1 || n > 7)
     {
-        Console.Write("Вы ошиблиись!\nВведите номер дня недели: ");
+        Console.Write("Вы ошиблись!\nВведите номер выходного дня недели: ");
         n = int.Parse(Console.ReadLine()!);
     }
-while (n == 6 || n == 7)
+if (n == 6 || n == 7)
     {
-        Console.WriteLine("Выходной!!!");
-        Console.Write("Введите номер дня недели: ");
-        n = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Да");
     }
-if (n == 1)
-    Console.WriteLine("Понедельник");
-else if (n == 2)
-    Console.WriteLine("Вторник");
-else if (n == 3)
-    Console.WriteLine("Среда");
-else if (n == 4)
-    Console.WriteLine("Четверг");
-else if (n == 5)
-    Console.WriteLine("Пятница");
-else if (n == 6)
-    Console.WriteLine("Суббота");
 else
-    Console.WriteLine("Воскресенье");
+    {
+        Console.WriteLine("Нет");
+    }

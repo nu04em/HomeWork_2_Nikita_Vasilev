24adb4d83fe16e90540900841503f2f178e5a819
Nikita <nikitarusvasilev@gmail.com>
2023-05-28 00:15:40 +0300

    done Task 10_copy

---
+++ Task10_copy/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+while (num < 100 || num > 999)
+    {
+        Console.Write("Вы ввели не трёхзначное число!\nВведите трёхзначное число: ");
+        num = int.Parse(Console.ReadLine()!);
+    }
+Console.WriteLine($"Второе число: " + (num % 100)/ 10);
+return;
